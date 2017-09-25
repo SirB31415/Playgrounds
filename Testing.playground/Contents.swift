309657e0841301743c69482ee416c2c1ba555c83
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-09-25 12:41:09 -0600

    Made a string interpolation and some comments for future components

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -3,3 +3,19 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myName :String
+var otherName = "Words"
+var thisName :String = "meow"
+
+// String Interpolation
+let Makai = (420.0/666.0)
+let Calvin = 666
+let Yael = Double(Calvin) * (Makai)
+let message = "\(Calvin) multiplied by \(Int(Makai)) is \(Int(Yael))"
+
+// String Change
+
+//Counting Characters
+
+// Comparing Strings
