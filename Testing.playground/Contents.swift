c7d78cae514625a4343a95927076f87fd91784e9
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-09-26 12:15:49 -0600

    Added some comments

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -11,11 +11,14 @@ var thisName :String = "meow"
 // String Interpolation
 let Makai = (420.0/666.0)
 let Calvin = 666
-let Yael = Double(Calvin) * (Makai)
+let Yael = Double(Calvin) * Double(Makai)
 let message = "\(Calvin) multiplied by \(Int(Makai)) is \(Int(Yael))"
-
+print(message)
+print("Trust me")
+print("It's gonna be uge")
 // String Change
 
 //Counting Characters
 
 // Comparing Strings
+
